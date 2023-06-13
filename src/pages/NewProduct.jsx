aeea567123e38ce27ed197f8e42c4f62c8e7ac0b
unknown <eodrmfdl1004@naver.com>
2023-06-13 12:34:36 +0900

    Feat: 장바구니, 제품 편집 리다이렉션

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,6 +1,15 @@
+import {useAuthContext} from "../context/AuthContext";
+import {Navigate} from "react-router-dom";
+
 import ProductForm from "../components/ProductForm";
 
 export default function NewProductPage() {
+  const {user} = useAuthContext();
+
+  if (!user?.isAdmin) {
+    return <Navigate to={'/'} replace={false}/>
+  }
+
   return (
     <section className='m-4'>
       <h1 className='pb-4 text-center text-2xl font-bold'>새로운 제품 등록</h1>
